430381c6d6d8ba10f3d6b32c32e1ee412df88a09
mingjie.neo <mingjie.neo@stengg.com>
2023-06-12 17:48:37 +0800

    Fix dependencies

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 const isPasswordEnabled = !!process.env.PASSWORD_PROTECT
-export async function middleware(req: NextRequest){
+export async function middleware(req){
     const isLoggedIn = req.cookies.has('login');
     const isPathPasswordProtect = req.nextUrl.pathname.startsWith("/password-protect")
 
